0694d12a43c36f69a63ac5e66e6e970c03c40aba
NathanH2111 <nathan@inhouseprinting.net>
2022-04-27 18:04:17 -0400

    Began working on Navbar and added all the required html pages

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ from flask import *
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def renderIndex():
    return render_template('index.html')
+
+@app.route("/login")
+def renderLogin():
+   return render_template('login.html')
+
+@app.route("/register")
+def renderRegister():
+   return render_template('register.html')
+
+@app.route("/shop")
+def renderShop():
+   return render_template('shop.html')
 
 if __name__ == '__main__':
    app.run(debug=True)
