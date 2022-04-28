58874cbb75a0ace3f4af4bad7295cfda44fe319b
Gabe-Dombach <gdombach009@gmail.com>
2022-04-28 08:46:44 -0400

    Added the symetric Key as well as the module for encryption and decryption and database connections

---
+++ data.py
@@ -0,0 +1,22 @@
+from cryptography.fernet import Fernet
+import psycopg2
+def connect(): return psycopg2.connect(
+     database = 'flask_db',
+            user = 'postgres',
+            password = 'gabe1972',
+            host = 'localhost'
+)
+
+def load_key():
+    return open("secret.key", "rb").read()
+
+def encrypt_text(text):
+    key = load_key()
+    f = Fernet(key)
+    return f.encrypt(bytes(text, "utf-8"))
+
+def decrypt_text(encrypted_text):
+    key = load_key()
+    f = Fernet(key)
+    decoded = f.decrypt(encrypted_text)
+    return decoded.decode("utf-8")
