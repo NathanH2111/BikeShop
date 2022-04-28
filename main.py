f057c4ec65ed1be00f1a2f43588ecf9d8d43f8a2
Gabe-Dombach <gdombach009@gmail.com>
2022-04-28 09:27:43 -0400

    added login and registration html

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import bcrypt
 import data 
 
 app = Flask(__name__)
+con = data.connect()
+cur = con.cursor()
+
+cur.execute('CREATE TABLE IF NOT EXISTS users (id BIGSERIAL PRIMARY KEY NOT NULL, email text NOT NULL, password BYTEA,role VARCHAR(5))')
+con.commit()
+cur.execute('CREATE TABLE IF NOT EXISTS bikeStock (name TEXT,type TEXT,price DOUBLE PRECISION,image text,description TEXT')
+cur.execute('CREATE TABLE IF NOT EXISTS bikesold (name TEXT,type TEXT,price DOUBLE PRECISION,image TEXT,description TEXT,customer BIGINT,FOREIGN KEY(customer) REFERENCES users(id))')
+con.commit()
+cur.close() 
+con.close()
 
 @app.route("/")
 def renderIndex():
