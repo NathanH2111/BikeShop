edcbd9a58e5d3cbff489237bc843bf433d26d627
Gabe-Dombach <gdombach009@gmail.com>
2022-05-10 09:23:03 -0400

    added nathan as an admin

--- data.py
+++ data.py
@@ -76,6 +76,7 @@ def initialInsert():
     pw = 'abc123'
     pw = pw.encode('utf-8')
     cur.execute('INSERT INTO users (email, address, password, role) VALUES(%s,%s,%s,%s) ON CONFLICT DO NOTHING;',('gmdombach211@stevenscollege.edu','4 spite rode',bcrypt.hashpw(pw,bcrypt.gensalt(10)),'A'))
+    cur.execute('INSERT INTO users (email, address, password, role) VALUES(%s,%s,%s,%s) ON CONFLICT DO NOTHING;',('nbhedemark211@stevenscollege.edu','4 spite rode',bcrypt.hashpw(pw,bcrypt.gensalt(10)),'A'))
     cur.execute('CREATE TABLE IF NOT EXISTS bikestock (name TEXT UNIQUE NOT NULL,type TEXT,price DOUBLE PRECISION,image text,description TEXT)')
     cur.execute('CREATE TABLE IF NOT EXISTS bikesold (name TEXT,type TEXT,price DOUBLE PRECISION,image TEXT,description TEXT,color varchar(7),gears INT,rimsize INT,  customer BIGINT,FOREIGN KEY(customer) REFERENCES users(id))')
     cur.executemany('INSERT INTO bikestock (name,type,price,image,description) VALUES(%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING',insertList,)
