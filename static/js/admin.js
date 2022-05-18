e0d8afab9a20af2fd0dc0525699d0d1ccca16a41
Gabe-Dombach <gdombach009@gmail.com>
2022-05-18 18:16:02 -0400

    added filters to admin inputs

---
+++ static/js/admin.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+ 
+    
+    $('.numbersOnly').keyup(function () {
+        if (this.value != this.value.replace(/[^0-9\.]/g, '')) {
+            this.value = this.value.replace(/[^0-9\.]/g, '');
+        }
+    });
+});
