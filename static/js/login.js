3800556db21f5c055c9c61790f1c19930dad2cfb
Gabe-Dombach <gdombach009@gmail.com>
2022-05-18 17:00:05 -0400

    fixed glitch with form submission diablement and disable context menue copy and cut on login and registration pages

--- static/js/login.js
+++ static/js/login.js
@@ -1,18 +1,28 @@
+
+
 $(document).ready(function () {
-    $form = $('#login'); // cache
-    $form.find(':input[type="submit"]').prop('disabled', true); // disable submit btn
-    $form.find(':input').change(function () {
-        // monitor all inputs for changes
-        var disable = false;
-        $form
-            .find(':input')
-            .not('[type="submit"]')
-            .each(function (i, el) {
-                // test all inputs for values
-                if ($.trim(el.value) === '') {
-                    disable = true; // disable submit if any of them are still blank
-                }
-            });
-        $form.find(':input[type="submit"]').prop('disabled', disable);
+          $('body').on('contextmenu', function (e) {
+              return false;
+          });
+     $('body').bind('cut copy', function (e) {
+         e.preventDefault();
+     });
+        $form = $('#login'); // cache
+        $form.find(':input[type="submit"]').prop('disabled', true); // disable submit btn
+        $('input').keyup(function () {
+            if (
+                $('#Password').val().trim() != '' && $('#Username').val().trim() != ''
+            ) {
+                console.log('fields filled');
+                console.log($('#ccn').length, $('#cvv').length);
+                $form.find(':input[type="submit"]').prop('disabled', false);
+            }
+        });
+        $('input').keydown(function () {
+            if ($('#Password').val().trim() == '' || $('#Username').val().trim() == '') {
+                console.log('some fields are empty!');
+                console.log($('#ccn').length, $('#cvv').length);
+                $form.find(':input[type="submit"]').prop('disabled', true);
+            }
+        });
     });
-});
